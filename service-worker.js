eec456a9c1dcdfc33ff9aa01b4087ec5ba9a4a5d
Андрей <nekr000100@mail.ru>
2020-02-04 18:21:47 +0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/SocialNetwork/precache-manifest.72e84dba1eadf0e4767e2ad3fe75741b.js"
+  "/SocialNetwork/precache-manifest.4712c02a5708853191f43ed6f2fde8d0.js"
 );
 
 self.addEventListener('message', (event) => {
